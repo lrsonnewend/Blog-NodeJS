4ddc62c9828a28005d6210341468c3f3e94e3373
lucas <lucas.lr184@gmail.com>
2019-09-25 20:47:19 -0300

    adicionando posts ao banco de dados

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
     const express = require("express");
     const handlebars = require("express-handlebars");
     const bodyParser = require("body-parser");
-    //const mongoose = require("mongoose");
+    const mongoose = require("mongoose");
     const app = express();
-
+    const admin = require("./routers/admin");
+    const path = require("path");
 //Configurações
     //Body Parser
         app.use(bodyParser.urlencoded({extended: true}));
@@ -15,10 +16,22 @@
         app.set("view engine", "handlebars");
 
     //Mongoose
+    mongoose.Promise = global.Promise;
+        mongoose.connect("mongodb://localhost/posts").then(() =>{
+            console.log("conectado ao mongo");
+        }).catch((erro) =>{
+            console.log("erro na conexao: "+erro);
+        })
+
+    //Public
+        app.use(express.static(path.join(__dirname,"public")));
+        
 
 
 
 //Rotas
+    app.use("/admin", admin);
+
 
 
 //Outros
